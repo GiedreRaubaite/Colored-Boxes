93b574ec23a4b8d5c3ac20470f33e207cfb9ada8
GiedreRaubaite <58292729+GiedreRaubaite@users.noreply.github.com>
2020-02-10 14:29:05 +0100

    done
    all 10 boxes done ;)

--- main.js
+++ main.js
@@ -2,3 +2,43 @@
 
   document.getElementById("box1").addEventListener("click", function () {
   box1.classList.add('bouncy-animation');})
+
+document.getElementById("box2").addEventListener("click", function () {
+  box2.classList.add('bounce');})
+
+document.getElementById("box3").addEventListener("click", function () {
+  box3.classList.add('slideOutDown');})
+
+document.getElementById("box4").addEventListener("click", function () {
+  box4.classList.add('rotateOut');})
+
+document.getElementById("box5").addEventListener("click", function () {
+  box5.classList.add('hinge');})
+
+document.getElementById("box6").addEventListener("click", function () {
+  box6.classList.add('background');})
+
+document.getElementById("box7").addEventListener("click", function () {
+  box7.classList.add('height');})
+
+document.getElementById("box8").addEventListener("click", function () {
+  box8.classList.add('erase');
+  box9.classList.add ('erase');})
+
+document.getElementById("box9").addEventListener("click", function () {
+  box8.classList.add('erase');
+  box9.classList.add ('erase');})
+
+document.getElementById("box10").addEventListener("click", function () {
+  box1.classList.add('height');
+box1.classList.add('height');
+box2.classList.add('height');
+box3.classList.add('height');
+box4.classList.add('height');
+box5.classList.add('height');
+box6.classList.add('height');
+box7.classList.add('height');
+box8.classList.add('height');
+box9.classList.add('height');
+box10.classList.add('height');
+  })
